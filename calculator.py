4b2b7abd2f2e1e8ed867e2ef0dc5fc0290500a65
Anant Vishwakarma <134936730+AnantVishwakarma2002@users.noreply.github.com>
2025-11-24 23:50:15 +0530

    feat: Implemented multiplication function

--- calculator.py
+++ calculator.py
@@ -5,6 +5,9 @@ def add(a, b):
 def subtract(a, b): 
     return a - b    
 
+def multiply(a, b): 
+    return a * b    
+
 if __name__ == "__main__":
     result = add(5, 3)
     print(f"5 + 3 = {result}")
